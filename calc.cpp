88d7fba689559f1ff306b7d114ca4a782603d00f
Vongola <vongola12324@coder.tw>
2015-05-17 22:53:10 +0800

    Add Point multiply in TotCoursePoint Calc

--- calc.cpp
+++ calc.cpp
@@ -5,7 +5,7 @@ void calcAverage(list<Student> &stu){
         int TotCredits = 0, TotCoursePoints = 0;
         for(list<_Course>::iterator it2 = it->Course.begin(); it2!=it->Course.end(); ++it2){
             TotCredits += it2->getPoint();
-            TotCoursePoints += it2->getScore();
+            TotCoursePoints += it2->getScore() * it2->getPoint();
         }
         it->setAvgData(TotCoursePoints, TotCredits);
     }
